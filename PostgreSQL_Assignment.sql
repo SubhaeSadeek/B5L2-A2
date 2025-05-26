307c4ade8c1f2a3ed5f8be7a39df039a928e0e94
SubhaeSadeek <nurun.nabi@outlook.com>
2025-05-26 17:07:55 +0600

    all 5 questions are solved with appropriate answer

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -46,6 +46,12 @@ INSERT INTO species(common_name, scientific_name, discovery_date, conservation_s
     ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
     ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
-INSERT INTO sightings()
+INSERT INTO `sightings`(ranger_id, species_id, sighting_time, location, notes )
     VALUES
-    ();
+    (2, 2, '2024-05-12 16:20:00', 'Bankwood Area' ,'Juvenile seen'),
+    (3, 3, '2024-05-15 09:10:00', 'Bamboo Grove East' ,'Feeding observed'),
+    (1, 2, '2024-05-18 18:30:00', 'Snowfall Pass', NULL);
+
+
+SELECT * FROM sightings;
+
