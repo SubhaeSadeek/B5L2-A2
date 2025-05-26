82b4be6a40df03000e224964aa3f169f825da6df
SubhaeSadeek <nurun.nabi@outlook.com>
2025-05-26 21:54:07 +0600

    solution is half way, upto 4 done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -21,10 +21,10 @@ conservation_status VARCHAR(20) NOT NULL
 
 CREATE TABLE sightings(
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INT NOT NULL,
     species_id INT NOT NULL,
-    sighting_time TIMESTAMP NOT NULL,
+    ranger_id INT NOT NULL,
     location VARCHAR(50),
+    sighting_time TIMESTAMP NOT NULL,
     notes TEXT,
     
     FOREIGN KEY(ranger_id) REFERENCES rangers(ranger_id),
@@ -46,12 +46,40 @@ INSERT INTO species(common_name, scientific_name, discovery_date, conservation_s
     ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
     ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
-INSERT INTO `sightings`(ranger_id, species_id, sighting_time, location, notes )
+INSERT INTO sightings(ranger_id, species_id, sighting_time, location, notes )
     VALUES
+
+    (1, 1, '2024-05-10 07:45:00', 'Peak Ridge' ,'Camera trap image captured'),
     (2, 2, '2024-05-12 16:20:00', 'Bankwood Area' ,'Juvenile seen'),
     (3, 3, '2024-05-15 09:10:00', 'Bamboo Grove East' ,'Feeding observed'),
     (1, 2, '2024-05-18 18:30:00', 'Snowfall Pass', NULL);
 
 
-SELECT * FROM sightings;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--PROBLEMS (TOTAL - 09)
+
+-- 01. Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+
+INSERT INTO rangers(name, region)
+    VALUES('Derek Fox', 'Coastal Plains');
+
+
+-- 02. Count unique species ever sighted.
+SELECT COUNT(DISTINCT species_id) AS  unique_species_count FROM sightings
+
+
+-- 03. Find all sightings where the location includes "Pass".
+SELECT * FROM sightings WHERE "location" LIKE '%Pass%'  ;
+
+-- 04. List each ranger's name and their total number of sightings.
+
+SELECT rangers.name, COUNT(sightings.species_id) AS  total_sightings
+FROM rangers
+JOIN sightings
+ON rangers.ranger_id = sightings.ranger_id
+GROUP BY rangers.name;
+
+
+select * FROM species;
 
